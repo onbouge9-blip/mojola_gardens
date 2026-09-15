26c54905264cb3b42ea39533a2003913f5a57df0
onbouge9-blip <onbouge9@gmail.com>
2026-09-15 03:36:29 +0100

    google verification

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ async function mojolaApiPost(endpoint, payload) {
     return { ok: false, offline: true }; // pas de réseau / backend injoignable
   }
 }
-window.mojolaApiPost = mojolaApiPost;google-site-verification=IGNhQlxBWj1BSAz3I8EUPRvMzGD1ZCSXH-t9r7n3sfg
+
 
 
 async function mojolaApiGet(endpoint, headers) {
